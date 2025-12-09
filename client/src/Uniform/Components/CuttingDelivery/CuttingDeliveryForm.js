6faae49a10fc7d24e360c8fd2f0f7283c2c9ffe2
logesh <logeshsmart72@gmail.com>
2025-12-09 17:08:38 +0530

    dec 9

--- client/src/Uniform/Components/CuttingDelivery/CuttingDeliveryForm.js
+++ client/src/Uniform/Components/CuttingDelivery/CuttingDeliveryForm.js
@@ -509,7 +509,7 @@ export default function CuttingDeliveryForm({
                     label: item?.ProcessGroupSeq?.name,
                     value: item.id,
                   }))}
-                readOnly={id}
+                readOnly={readOnly}
                 required={true}
                 placeholder="Select Group"
                 onKeyDown={(e) => {
